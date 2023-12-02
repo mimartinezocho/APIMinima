c9cbfc4856d88b1a5d2ec0ed542970f700282129
Miguel <mimartinezocho@gmail.com>
2023-12-02 13:42:48 -0600

    Agregar archivos de proyecto.

---
+++ APIMinima/TodoItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace APIMinima
+{
+    public class TodoItemDTO
+    {
+        public int id {  get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public TodoItemDTO() { }
+        public TodoItemDTO(Todo todoitem) => 
+            (id, Name, IsComplete)=(todoitem.id, todoitem.Name, todoitem.IsComplete);
+    }
+}
